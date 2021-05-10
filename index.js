659161c906f18dba790fa522e2cb6d4a5197c71f
prashant220 <ppoudel624@gmail.com>
2021-05-10 16:49:31 +0545

    changed

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 
+var xposition = 1;
+var yposition = 1;
+var counter = 0;
+var div = new Array();
+var Xposition = new Array();
+var Yposition = new Array();
+var para= document.getElementById("inner-text");
+var text=document.createElement('p');
+text.innerHTML="ants killed:0"
+para.appendChild(text)
+
 
 window.onload = createDiv();
 
@@ -11,21 +22,42 @@ function createDiv() {
       X +
       "px; left:" +
       Y +
-      "px; border-radius:50%";
+      "px; border-radius:50%;"
+      var ant = document.createElement("img");
+      ant.src = "ant-svg.svg";
+      ant.setAttribute("style", "width:30px; height:30px;");
+      divs.appendChild(ant);
+      
 
     var X = Math.floor(Math.random() * 420 + 1);
     var Y = Math.floor(Math.random() * 420 + 1);
 
-    const randomColor = Math.floor(Math.random() * 16777215).toString(16);
-    divs.style.background = "#" + randomColor;
+    // const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+    // divs.style.background = "#" + randomColor;
 
     const maindiv = document.getElementById("container");
     maindiv.append(divs);
+   divs.addEventListener('click',function(){
+     deleteBoxes(this)
+     text.innerHTML="ants killed:"+counter
+    
+   });
+      
 
-    Movement(0.5, 0.5, X, Y, divs);
+    Movement(0.2, 0.2, X, Y, divs,i);
+  
   }
+  function deleteBoxes(el){
+    counter++;
+    var element = el;
+    element.remove();
+    
+   
+  }
+ 
+  
 
-  function Movement(xposition, yposition, X, Y, divs) {
+  function Movement(xposition, yposition, X, Y, divs,index) {
     setInterval(() => {
       X += xposition;
       divs.style.left = X + "px";
@@ -39,6 +71,18 @@ function createDiv() {
     if (Y > 440 || Y < 0) {
         yposition = yposition * -1;
     }
+   
+    Xposition[index] = X;
+    Yposition[index] = Y;
+    for (var i = 0; i < 5; i++) {
+
+      if (!(i === index)) {
+          if ((Xposition[i] < X + 30) && (Xposition[i] + 30 > X) && (Yposition[i] < Y + 30) && (Yposition[i] + 30 > Y)) {
+              xposition = xposition * -1;
+              yposition = yposition * -1;
+          }
+      }
+  }
     }, 1);
   }
 }
